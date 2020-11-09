9027ca3fbaf7b06471010b1a0970a9e8abc601bd
sriibm <42212963+sriibm@users.noreply.github.com>
2020-11-09 12:08:45 -0700

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,20 @@
 	<h1>PHP Test</h1>
 		<p><b>An Example of PHP in Action - Sridhar - NEW on Nov 9, 2020 - Can I change THIS????</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
-		echo date("g:i A l, F j Y.");
-		$_SERVER['SERVER_ADDR']
-		echo $_SERVER ?> </p>
+		echo date("g:i A l, F j Y.");?> </p>
+	
+<p>
+// PHP program to obtain IP address of 
+// the server 
+  
+// Creating a variable to store the 
+// server address 
+$ip_server = $_SERVER['SERVER_ADDR']; 
+  
+// Printing the stored address 
+echo "Server IP Address is: $ip_server"; 
+?></p> 
+	
 	<h2>PHP Information</h2> 
 		<p> <?php phpinfo(); ?> </p> 
 	</body> 
